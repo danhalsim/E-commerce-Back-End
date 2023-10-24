3a2acb10b52f732571933987546141d5afb0763a
dansim6935 <dansim6935@gmail.com>
2023-10-23 20:57:30 -0700

    Finished server.js and models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE' 
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id',
+    unique: false,
+    onDelete: 'SET NULL'
+  }
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id',
+    unique: false,
+    onDelete: 'SET NULL'
+  }
+})
+
 
 module.exports = {
   Product,
